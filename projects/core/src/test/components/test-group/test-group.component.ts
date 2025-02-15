d44a830a70f3d529bc745a4ee8ee0859f903d062
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-15 11:25:46 +0100

    fix: standardized input name for controls and form groups

--- projects/core/src/test/components/test-group/test-group.component.ts
+++ projects/core/src/test/components/test-group/test-group.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { NgxFwFormGroup } from '../../../lib/types/content.type';
 
 @Component({
   selector: 'ngxfw-test-group',
   imports: [],
   templateUrl: './test-group.component.html',
 })
-export class TestGroupComponent {}
+export class TestGroupComponent {
+  readonly content = input<NgxFwFormGroup>();
+  readonly testId = computed(() => {
+    const group = this.content();
+    if (!group || !group.id) {
+      return 'group';
+    }
+    return `group-${group.id}`;
+  });
+}
--- projects/core/src/test/components/test-text-control/test-text-control.component.ts
+++ projects/core/src/test/components/test-text-control/test-text-control.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { NgxFwControl } from '../../../lib/types/content.type';
 
 @Component({
   selector: 'ngxfw-test-text-control',
   imports: [],
   templateUrl: './test-text-control.component.html',
 })
-export class TestTextControlComponent {}
+export class TestTextControlComponent {
+  readonly content = input<NgxFwControl>();
+
+  readonly testId = computed(() => {
+    const control = this.content();
+    if (!control || !control.id) {
+      return 'control';
+    }
+    return `control-${control.id}`;
+  });
+}
