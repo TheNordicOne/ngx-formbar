2fe3bd4cfe01b26ce2d28ca6c09a293dd53c8d5d
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-15 10:46:24 +0100

    refactor: extracted formwork provider for test setup

--- projects/core/src/test/integration/content-registration/content-registration.cy.ts
+++ projects/core/src/test/integration/content-registration/content-registration.cy.ts
@@ -1,23 +1,10 @@
 import { RegistrationIntegrationHostComponent } from './integration-host/registration-integration-host.component';
-import { provideFormwork } from '../../../lib';
-import { TestTextControlComponent } from '../../components/test-text-control/test-text-control.component';
-import { TestGroupComponent } from '../../components/test-group/test-group.component';
+import { formworkProviders } from '../shared/provide-formwork';
 
 describe('Content Registration', () => {
   it('should show registered content', () => {
     cy.mount(RegistrationIntegrationHostComponent, {
-      providers: [
-        provideFormwork(
-          {
-            type: 'test-text-control',
-            component: TestTextControlComponent,
-          },
-          {
-            type: 'test-group',
-            component: TestGroupComponent,
-          },
-        ),
-      ],
+      providers: [formworkProviders],
       componentProperties: {
         content: {
           type: 'test-text-control',
