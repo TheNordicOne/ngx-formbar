243814e4f90f1497a1c8e6d0ceeca0685e19b6e4
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-15 14:39:14 +0100

    feat: added export for content types

--- projects/core/src/lib/index.ts
+++ projects/core/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './components/form/ngx-fw-form.component';
+export * from './types/content.type';
 export * from './types/registration.type';
 export * from './config/provide-formfwork';
 export * from './directives/ngxfw-control.directive';
