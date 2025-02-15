9415fa344bda27f2196d7c1f00f42070030cc59e
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-15 11:38:46 +0100

    feat: implemented form component

--- projects/core/src/lib/components/form/form.component.cy.ts
+++ projects/core/src/lib/components/form/form.component.cy.ts
@@ -1,7 +1,21 @@
 import { NgxFwFormComponent } from './ngx-fw-form.component';
+import { ContentRegistrationService } from '../../services/content-registration.service';
+import { Type } from '@angular/core';
 
 describe('Form Component', () => {
   it('it should create the component', () => {
-    cy.mount(NgxFwFormComponent);
+    cy.mount(NgxFwFormComponent, {
+      providers: [
+        {
+          provide: ContentRegistrationService,
+          useValue: new ContentRegistrationService(
+            new Map<string, Type<unknown>>(),
+          ),
+        },
+      ],
+      componentProperties: {
+        formContent: [],
+      },
+    });
   });
 });
