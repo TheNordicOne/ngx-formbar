bb88701d67b61c25d44f01a798f5454c2ec27a6c
Alexander Pahn <alexander.pahn@outlook.com>
2025-03-09 10:45:15 +0100

    fix: fixed tests

--- projects/core/src/lib/services/validator-registration.service.spec.ts
+++ projects/core/src/lib/services/validator-registration.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValidatorRegistrationService } from './validator-registration.service';
+import { AsyncValidatorFn, ValidatorFn } from '@angular/forms';
 
 describe('ValidatorRegistrationService', () => {
   let service: ValidatorRegistrationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ValidatorRegistrationService,
+          useValue: new ValidatorRegistrationService(
+            new Map<string, ValidatorFn[]>(),
+            new Map<string, AsyncValidatorFn[]>(),
+          ),
+        },
+      ],
+    });
     service = TestBed.inject(ValidatorRegistrationService);
   });
 
