013833bd897a6c0daad501623d6b5e9a8268823d
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-12 12:52:42 +0200

    refactor: move type helper and expose them

--- projects/core/src/lib/index.ts
+++ projects/core/src/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './components/form/ngx-fw-form.component';
 export * from './types/content.type';
 export * from './types/registration.type';
+export * from './types/helper.type';
 export * from './config/provide-formwork';
 export * from './directives/ngxfw-control.directive';
 export * from './directives/ngxfw-group.directive';
