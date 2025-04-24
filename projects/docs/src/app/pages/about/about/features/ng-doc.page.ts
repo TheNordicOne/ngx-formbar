f754b5c1e8297577aa9c6271ec32770f536f2bbc
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-24 13:23:53 +0200

    docs: add feature page

---
+++ projects/docs/src/app/pages/about/about/features/ng-doc.page.ts
@@ -0,0 +1,10 @@
+import { NgDocPage } from '@ng-doc/core';
+import AboutCategory from '../ng-doc.category';
+
+const CurrentStatePage: NgDocPage = {
+  title: `Features`,
+  mdFile: './index.md',
+  category: AboutCategory,
+};
+
+export default CurrentStatePage;
---
+++ projects/docs/src/app/pages/about/about/ng-doc.category.ts
@@ -0,0 +1,8 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const AboutCategory: NgDocCategory = {
+  title: 'About',
+  order: 0,
+};
+
+export default AboutCategory;
