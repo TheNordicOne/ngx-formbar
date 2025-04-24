97197fa02607c17fe6a2040eee3e2016cac99cab
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-24 13:15:27 +0200

    docs: add about page

---
+++ projects/docs/src/app/pages/getting-started/what-is-ngx-formwork/ng-doc.page.ts
@@ -0,0 +1,10 @@
+import { NgDocPage } from '@ng-doc/core';
+import GettingStartedCategory from '../ng-doc.category';
+
+const WhatIsNgxFormworkPage: NgDocPage = {
+  title: `What is ngx-formwork?`,
+  mdFile: './index.md',
+  category: GettingStartedCategory,
+};
+
+export default WhatIsNgxFormworkPage;
