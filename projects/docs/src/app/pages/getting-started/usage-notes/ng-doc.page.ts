ee0b1a96d93d67a6488a157697427c4f716473eb
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-24 14:53:36 +0200

    docs: add usage notes page

---
+++ projects/docs/src/app/pages/getting-started/usage-notes/ng-doc.page.ts
@@ -0,0 +1,10 @@
+import { NgDocPage } from '@ng-doc/core';
+import GettingStartedCategory from '../ng-doc.category';
+
+const UsageNotesPage: NgDocPage = {
+  title: `Usage Notes`,
+  mdFile: './index.md',
+  category: GettingStartedCategory,
+};
+
+export default UsageNotesPage;
