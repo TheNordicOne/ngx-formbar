e37c98a886f567082c0a8df33bc29c1610033771
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-25 08:56:30 +0200

    docs: add Minimal Setup documentation

---
+++ projects/docs/src/app/pages/getting-started/minimal-setup/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import GettingStartedCategory from '../ng-doc.category';
+
+const MinimalSetupPage: NgDocPage = {
+  title: `Minimal Setup`,
+  mdFile: './index.md',
+  category: GettingStartedCategory,
+  order: 2,
+};
+
+export default MinimalSetupPage;
