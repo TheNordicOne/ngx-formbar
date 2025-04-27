f27097fa1de1df8698e3911d246995e07ecc16dc
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-27 10:14:27 +0200

    docs: add showing a form page

---
+++ projects/docs/src/app/pages/guides/showing/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import GuidesCategory from '../ng-doc.category';
+
+const ShowingPage: NgDocPage = {
+  title: `Showing a Form`,
+  mdFile: './index.md',
+  category: GuidesCategory,
+  order: 3,
+};
+
+export default ShowingPage;
