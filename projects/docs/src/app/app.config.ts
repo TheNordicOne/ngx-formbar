6303f5ee7dfaaf9bffffab7ff966771898bfe40e
Alexander Pahn <alexander.pahn@outlook.com>
2025-04-27 13:50:14 +0200

    feat: enhance app layout and navigation with GitHub link and routing

--- projects/docs/src/app/app.config.ts
+++ projects/docs/src/app/app.config.ts
@@ -18,7 +18,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -38,6 +41,7 @@ export const appConfig: ApplicationConfig = {
     provideNgDocApp(),
     provideSearchEngine(NgDocDefaultSearchEngine),
     providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON),
-    provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS), provideClientHydration(withEventReplay()),
+    provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS),
+    provideClientHydration(withEventReplay()),
   ],
 };
--- projects/docs/src/app/app.routes.ts
+++ projects/docs/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'about/what-is-ngx-formwork',
+  },
+];
