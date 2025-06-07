a0b1c751de53d1b34fb9dbad9e6d81b67142bed7
Alexander Pahn <alexander.pahn@outlook.com>
2025-06-07 11:36:42 +0200

    fix: fix some typing and remove export

--- projects/core/src/lib/index.ts
+++ projects/core/src/lib/index.ts
@@ -1,7 +1,6 @@
 export * from './components/form/ngx-fw-form.component';
 export * from './types/content.type';
 export * from './types/registration.type';
-export * from './types/helper.type';
 export * from './config/provide-formwork';
 export * from './directives/ngxfw-control.directive';
 export * from './directives/ngxfw-group.directive';
