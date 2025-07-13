d683c5d9926a3fe60cb24ac80f7754c7c3b504a1
Alexander Pahn <alexander.pahn@outlook.com>
2025-07-13 12:13:20 +0200

    feat: improve ng-add schematic

--- projects/core/schematics/ng-add/ng-add.spec.ts
+++ projects/core/schematics/ng-add/ng-add.spec.ts
@@ -3,6 +3,8 @@ import {
   UnitTestTree,
 } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
+import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
+import { Schema as ApplicationOptions } from '@schematics/angular/application/schema';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 const runner = new SchematicTestRunner('ngx-formwork', collectionPath);
@@ -12,23 +14,27 @@ describe('ng-add schematic', () => {
 
   beforeEach(async () => {
     // Create a mock workspace + app
+    const workspaceOptions: WorkspaceOptions = {
+      name: 'workspace',
+      version: '19.0.0',
+      newProjectRoot: 'projects',
+    };
+
     appTree = await runner.runExternalSchematic(
       '@schematics/angular',
       'workspace',
-      {
-        name: 'workspace',
-        version: '19.0.0',
-        newProjectRoot: 'projects',
-      },
+      workspaceOptions,
     );
+
+    const appOptions: ApplicationOptions = {
+      name: 'app',
+      standalone: true,
+    };
 
     appTree = await runner.runExternalSchematic(
       '@schematics/angular',
       'application',
-      {
-        name: 'app',
-        standalone: true,
-      },
+      appOptions,
       appTree,
     );
   });
@@ -36,6 +42,14 @@ describe('ng-add schematic', () => {
   it('should generate formwork.config.ts', async () => {
     const tree = await runner.runSchematic('ng-add', {}, appTree);
     expect(tree.files).toContain('/projects/app/src/app/formwork.config.ts');
+
+    const configContent = tree.readContent(
+      '/projects/app/src/app/formwork.config.ts',
+    );
+    expect(configContent).toContain('import { defineFormworkConfig } from');
+    expect(configContent).toContain(
+      'export const formworkConfig = defineFormworkConfig',
+    );
   });
 
   it('should update app.config.ts', async () => {
@@ -45,5 +59,6 @@ describe('ng-add schematic', () => {
     expect(content).toContain(
       "import { formworkConfig } from './formwork.config'",
     );
+    expect(content).toContain("import { provideFormwork } from 'ngx-formwork'");
   });
 });
