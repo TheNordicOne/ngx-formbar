322ad17c72a4a50b5df8901d185e1f2159efb84a
Alexander Pahn <alexander.pahn@outlook.com>
2025-07-26 13:45:06 +0200

    feat: add schematic for scaffolding a block

---
+++ projects/core/schematics/block/index.ts
@@ -0,0 +1,9 @@
+import { chain, Rule } from '@angular-devkit/schematics';
+import { scaffoldAndRegister } from '../shared/rules';
+import { Schema } from '../shared/schema';
+
+export function block(options: Schema): Rule {
+  return () => {
+    return chain([scaffoldAndRegister(options)]);
+  };
+}
