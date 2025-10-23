312893acfdc722035eee850e0c18c568a949fc64
Alexander Pahn <alexander.pahn@outlook.com>
2025-10-23 12:04:51 +0200

    refactor: rename scaffold-and-register rule file

--- projects/core/automation/schematics/control/index.ts
+++ projects/core/automation/schematics/control/index.ts
@@ -1,5 +1,5 @@
 import { chain, Rule } from '@angular-devkit/schematics';
-import { scaffoldAndRegister } from '../shared/rules/rules';
+import { scaffoldAndRegister } from '../shared/rules/scaffold-and-register.rule';
 import { Schema } from '../shared/schema';
 
 export function control(options: Schema): Rule {
