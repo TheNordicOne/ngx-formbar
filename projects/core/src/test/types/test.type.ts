f19a8899a8b85c59dbec9c8307eb2ab630d653d7
Alexander Pahn <alexander.pahn@outlook.com>
2025-10-26 14:33:37 +0100

    chore: remove unused type playground

--- projects/core/src/test/types/test.type.ts
+++ projects/core/src/test/types/test.type.ts
@@ -1,40 +1,4 @@
 import { TestTextControl } from './controls.type';
 import { TestGroup } from './group.type';
 
-// Playground for testing out types
 export type MyAppControls = TestTextControl | TestGroup;
-
-export const someControls: MyAppControls[] = [
-  {
-    id: 'first',
-    type: 'test-text-control',
-    label: 'First',
-    defaultValue: 'default-first',
-  },
-  {
-    type: 'test-group',
-    id: 'first-group',
-    title: 'First Group',
-    controls: [
-      {
-        id: 'grouped-first',
-        type: 'test-text-control',
-        label: 'Grouped First label',
-        defaultValue: 'default-grouped-first',
-      },
-      {
-        type: 'test-group',
-        id: 'nested-group',
-        title: 'Nested Group',
-        controls: [
-          {
-            id: 'nested-second',
-            type: 'test-text-control',
-            label: 'Nested Second label',
-            defaultValue: 'default-nested-second',
-          },
-        ],
-      },
-    ],
-  },
-];
