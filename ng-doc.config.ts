1364c028e680694d60a80f9037c6dbcb2be77eb7
Alexander Pahn <alexander.pahn@outlook.com>
2026-02-08 12:56:11 +0100

    docs: improve docs setup

--- ng-doc.config.ts
+++ ng-doc.config.ts
@@ -2,6 +2,11 @@ import { NgDocConfiguration } from '@ng-doc/builder';
 
 const config: NgDocConfiguration = {
   docsPath: 'projects/docs/src/docs',
+  repoConfig: {
+    url: 'https://github.com/TheNordicOne/ngx-formbar',
+    mainBranch: 'main',
+    releaseBranch: 'main',
+  },
 };
 
 export default config;
--- projects/docs/src/app/app.ts
+++ projects/docs/src/app/app.ts
@@ -2,8 +2,10 @@ import {
   NgDocNavbarComponent,
   NgDocRootComponent,
   NgDocSidebarComponent,
+  NgDocThemeToggleComponent
 } from '@ng-doc/app';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+import { httpResource } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -13,10 +15,18 @@ import { RouterOutlet } from '@angular/router';
     NgDocRootComponent,
     NgDocNavbarComponent,
     NgDocSidebarComponent,
+    NgDocThemeToggleComponent,
   ],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('docs');
+  // eslint-disable-next-line @angular-eslint/no-experimental
+  private readonly npmResource = httpResource<{ version: string }>(
+    () => 'https://registry.npmjs.org/@ngx-formbar/core/latest',
+  );
+  protected readonly latestVersion = computed(() => {
+    const data = this.npmResource.value();
+    return data ? `v${data.version}` : '';
+  });
 }
