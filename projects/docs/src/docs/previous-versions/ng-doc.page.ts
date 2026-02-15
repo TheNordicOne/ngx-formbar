f09b8dc1584779faaa96257ff6ee6554f430da6f
Alexander Pahn <alexander.pahn@outlook.com>
2026-02-15 16:03:26 +0100

    docs: add page for previous versions

---
+++ projects/docs/src/docs/previous-versions/ng-doc.page.ts
@@ -0,0 +1,9 @@
+import { NgDocPage } from '@ng-doc/core';
+
+const PreviousVersionsPage: NgDocPage = {
+  title: 'Previous Versions',
+  mdFile: './index.md',
+  order: 6,
+};
+
+export default PreviousVersionsPage;
