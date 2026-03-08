70b526ce8739c40bcc28c985f97f50eda71d95d5
Alexander Pahn <alexander.pahn@outlook.com>
2026-03-08 09:44:24 +0100

    chore: initialize setup library

---
+++ libs/setup/src/lib/setup.spec.ts
@@ -0,0 +1,7 @@
+import { setup } from './setup';
+
+describe('setup', () => {
+  it('should work', () => {
+    expect(setup()).toEqual('setup');
+  });
+});
---
+++ libs/setup/src/lib/setup.ts
@@ -0,0 +1,3 @@
+export function setup(): string {
+  return 'setup';
+}
