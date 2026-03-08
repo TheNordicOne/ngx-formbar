b04d6915e392dba1a524bfbd21e4bea89dd677d1
Alexander Pahn <alexander.pahn@outlook.com>
2026-03-08 09:37:06 +0100

    chore: initialize examples library

---
+++ libs/examples/README.md
@@ -0,0 +1,7 @@
+# @ngx-formbar/examples
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @ngx-formbar/examples` to execute the unit tests.
---
+++ libs/examples/src/lib/examples/examples.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ngxfb-ngx-formbar-examples',
+  imports: [],
+  templateUrl: './examples.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NgxFormbarExamples {}
