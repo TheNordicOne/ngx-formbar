b44f9dc21d7c91c4cbd6af45d653d059f34baa56
Alexander Pahn <alexander.pahn@outlook.com>
2026-06-13 09:41:12 +0200

    chore: ensure storybook test runner doesn't get confused by verdaccios temp dir

--- test-runner-jest.config.cjs
+++ test-runner-jest.config.cjs
@@ -4,5 +4,5 @@ const testRunnerConfig = getJestConfig();
 
 module.exports = {
   ...testRunnerConfig,
-  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/.nx/cache/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/.nx/cache/', '<rootDir>/tmp/'],
 };
